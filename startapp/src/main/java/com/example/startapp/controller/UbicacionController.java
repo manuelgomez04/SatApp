8ec79c0325d457a1c7908b78e7745719529655c9
Victor <medina.levic24@triana.salesianos.edu>
2025-02-03 13:08:01 +0100

    doc save ubicacion

--- startapp/src/main/java/com/example/startapp/controller/UbicacionController.java
+++ startapp/src/main/java/com/example/startapp/controller/UbicacionController.java
@@ -1,7 +1,15 @@
 package com.example.startapp.controller;
 
+import com.example.startapp.dto.EditNotaDto;
 import com.example.startapp.dto.GetUbicacionDto;
 import com.example.startapp.service.UbicacionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +25,19 @@ public class UbicacionController {
 
     private final UbicacionService ubicacionService;
 
+    @Operation(summary = "Crea una ubicacion")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha creado la ubicacion",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = GetUbicacionDto.class)),
+                            examples = {@ExampleObject(
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha creado la ubicacion",
+                    content = @Content),
+    })
     @PostMapping
     public ResponseEntity<GetUbicacionDto> saveUbicacion(@RequestBody GetUbicacionDto nuevo) {
         return ResponseEntity.status(HttpStatus.CREATED).body(GetUbicacionDto.of(ubicacionService.saveUbicacion(nuevo)));
