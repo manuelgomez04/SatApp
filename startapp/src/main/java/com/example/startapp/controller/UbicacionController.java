bfb545954c8fb020de6b34abf2ea05d2c17a66ee
Victor <medina.levic24@triana.salesianos.edu>
2025-02-03 13:22:27 +0100

    tag name controller

--- startapp/src/main/java/com/example/startapp/controller/UbicacionController.java
+++ startapp/src/main/java/com/example/startapp/controller/UbicacionController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/ubicacion")
+@Tag(name = "Ubicacion",description = "Controlador para ubicaciones")
 public class UbicacionController {
 
     private final UbicacionService ubicacionService;
